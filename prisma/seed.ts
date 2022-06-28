e7d4eaf530bdb331ccba30a8e2f2db53b89cbac3
Zachary Wolfson <zwolfson@hubspot.com>
2022-06-28 19:40:56 -0400

    more company property examples

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -54,6 +54,55 @@ async function main() {
             object: "Company"
         }
     })
+
+    const companyName = await prisma.properties.upsert({
+        where: {
+            name_object: {
+                name: "name",
+                object: "Company"
+            }
+        },
+        update: {},
+        create: {
+            name: "name",
+            label: "Name",
+            type: "String",
+            object: "Company"
+        }
+    }
+    )
+
+    const industry = await prisma.properties.upsert({
+        where: {
+            name_object: {
+                name: "industry",
+                object: "Company"
+            }
+        },
+        update: {},
+        create: {
+            name: "industry",
+            label: "Industry",
+            type: "String",
+            object: "Company"
+        }
+    })
+    const num_employees = await prisma.properties.upsert({
+        where: {
+            name_object: {
+                name: "num_employees",
+                object: "Company"
+            }
+        },
+        update: {},
+        create: {
+            name: "num_employees",
+            label: "Number of Employees",
+            type: "Number",
+            object: "Company"
+        }
+    }
+    )
 }
 
 main()
