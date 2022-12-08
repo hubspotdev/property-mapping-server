8bef8717c356530e672ed47bc1dda72109229e79
Zachary Wolfson <zwolfson@hubspot.com>
2022-12-08 15:16:19 -0500

    back up to feature parity

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,9 @@ import {
   createPropertyGroup,
   createRequiredProperty,
 } from "./properties";
-import { saveMappings, getMappings, deleteMapping } from "./mappings";
+import { saveMapping, getMappings, deleteMapping } from "./mappings";
 import { PORT, getCustomerId } from "./utils";
-import { Mapping } from "default";
+import { Mapping, Properties } from "@prisma/client";
 
 const app: Application = express();
 app.use(express.json());
@@ -48,27 +48,50 @@ app.get("/api/native-properties/", async (req: Request, res: Response) => {
   res.send(properties);
 });
 
+app.get(
+  "/api/native-properties-with-mappings",
+  async (req: Request, res: Response) => {
+    const customerId = getCustomerId();
+    const properties: Properties[] = await getNativeProperties(customerId);
+    const mappings: Mapping[] = await getMappings(customerId);
+    const propertiesWithMappings = properties.map((property) => {
+      const matchedMapping = mappings.find(
+        (mapping) => mapping.nativeName == property.name
+      );
+      return { property, mapping: matchedMapping };
+    });
+    res.send(propertiesWithMappings);
+  }
+);
+
 app.post("/api/mappings", async (req: Request, res: Response) => {
-  const response = await saveMappings(req.body as Mapping[]);
+  const response = await saveMapping(req.body as Mapping);
   console.log("mapping save response", response);
+  if (response instanceof Error) {
+    res.status(500).send("Unkown Error");
+  }
   res.send(response);
 });
 
 app.delete("/api/mappings/:mappingId", async (req: Request, res: Response) => {
   const mappingToDelete = req.params.mappingId;
   const mappingId = parseInt(mappingToDelete);
+  if (!mappingId || mappingId === NaN) {
+    res.status(400).send("Invalid mapping Id format");
+  }
 
   const deleteMappingResult = await deleteMapping(mappingId);
   res.send(deleteMappingResult);
 });
 
 app.get("/api/mappings", async (req: Request, res: Response) => {
-  const mappings = await getMappings();
+  const customerId = getCustomerId();
+  const mappings = await getMappings(customerId);
   const formattedMappings = mappings.map((mapping) => {
-    const { name, hubspotLabel, hubspotName, id, object } = mapping;
+    const { nativeName, hubspotLabel, hubspotName, id, object } = mapping;
     return {
       id,
-      name,
+      nativeName,
       property: { name: hubspotName, label: hubspotLabel, object },
     };
   });
