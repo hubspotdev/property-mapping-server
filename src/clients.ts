9798895dd2ac74951796e6c072f2ca66528c6624
Zach Radford <zradford@hubspot.com>
2024-09-18 14:16:25 -0400

    added error handling and prisma logs to client.ts

--- src/clients.ts
+++ src/clients.ts
@@ -1,7 +1,10 @@
 import { Authorization, PrismaClient, Objects } from "@prisma/client";
 import * as hubspot from "@hubspot/api-client";
 
-const prisma = new PrismaClient();
+//Should be refactored to a single PrismaClient instance
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
 const hubspotClient = new hubspot.Client({});
 
 export { prisma, hubspotClient };
