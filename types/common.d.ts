64152eeb4d70134db658f0e2bb461e10667fa613
Zach Radford <zradford@hubspot.com>
2024-09-30 11:06:14 -0400

    added LogMessage and LogObject types

--- types/common.d.ts
+++ types/common.d.ts
@@ -47,6 +47,25 @@ declare module "default" {
     readOnlyValue: boolean;
   }
 
+  export interface LogMessage {
+    message: string;
+    object?: any;
+    context?:string;
+    data?: any;
+    stack?: string;
+    code?: string;
+    statusCode?: number;
+    correlationId?: string;
+    details?: any[];
+    error?: Error
+}
+
+ export interface LogObject {
+    logMessage : LogMessage,
+    critical? : boolean,
+    context : string,
+    type? : string
+}
 
   //fix unused interface
   export interface HubSpotProperty {
